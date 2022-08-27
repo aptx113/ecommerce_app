c7b72afb5050b2192070e71b387667bf3a37ea22
George <george.hs@hsress.com>
2022-08-27 09:50:43 +0800

    Implement InMemoryStore to save authState

---
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../utils/in_memory_store.dart';
+import '../models/app_user.dart';
+
+abstract class AuthRepository {
+  Stream<AppUser?> authStateChanges();
+  AppUser? get currentUser;
+  Future<void> signInWithEmailAndPassword(String email, String password);
+  Future<void> createUserWithEmailAndPassword(String email, String password);
+  Future<void> signOut();
+}
+
+class FakeAuthRepository implements AuthRepository {
+  final _authState = InMemoryStore<AppUser?>(null);
+
+  @override
+  Stream<AppUser?> authStateChanges() => _authState.stream;
+  @override
+  AppUser? get currentUser => _authState.value;
+
+  @override
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
+  }
+
+  @override
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password) async {
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
+  }
+
+  @override
+  Future<void> signOut() async {
+    _authState.value = null;
+  }
+
+  void dispose() => _authState.close();
+
+  void _createNewUser(String email) {
+    _authState.value =
+        AppUser(uid: email.split('').reversed.join(), email: email);
+  }
+}
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  final authRepo = FakeAuthRepository();
+  ref.onDispose(() => authRepo.dispose());
+  return authRepo;
+});
+
+final authStateChangesProvider = StreamProvider.autoDispose<AppUser?>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return authRepository.authStateChanges();
+});
---
+++ lib/src/utils/in_memory_store.dart
@@ -0,0 +1,14 @@
+import 'package:rxdart/subjects.dart';
+
+class InMemoryStore<T> {
+  InMemoryStore(T initial) : _subject = BehaviorSubject<T>.seeded(initial);
+  final BehaviorSubject<T> _subject;
+
+  Stream<T> get stream => _subject.stream;
+
+  T get value => _subject.value;
+
+  set value(T value) => _subject.value;
+
+  void close() => _subject.close();
+}
