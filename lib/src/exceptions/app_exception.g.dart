542dc1961f86e07c54505bcc96b6d03011e46e18
George <george.hs@hsress.com>
2022-09-19 20:25:45 +0800

    Use Freezed to generate exception data class

---
+++ lib/src/exceptions/app_exception.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_exception.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$PermissionDenied _$$PermissionDeniedFromJson(Map<String, dynamic> json) =>
+    _$PermissionDenied(
+      $type: json['runtimeType'] as String?,
+    );
+
+Map<String, dynamic> _$$PermissionDeniedToJson(_$PermissionDenied instance) =>
+    <String, dynamic>{
+      'runtimeType': instance.$type,
+    };
+
+_$PaymentFailed _$$PaymentFailedFromJson(Map<String, dynamic> json) =>
+    _$PaymentFailed(
+      $type: json['runtimeType'] as String?,
+    );
+
+Map<String, dynamic> _$$PaymentFailedToJson(_$PaymentFailed instance) =>
+    <String, dynamic>{
+      'runtimeType': instance.$type,
+    };
