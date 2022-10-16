289f01abfb22631c6f64b660659381b8ac136202
George <george.hs@hsress.com>
2022-10-16 18:33:35 +0800

    implement cart service

---
+++ lib/src/features/cart/application/cart_service.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../authentication/data/fake_auth_repository.dart';
+import '../../products/models/product.dart';
+import '../data/local/local_cart_repository.dart';
+import '../data/remote/remote_cart_repository.dart';
+import '../models/cart.dart';
+import '../models/item.dart';
+import '../models/mutable_cart.dart';
+
+class CartService {
+  CartService(this.ref);
+  final Ref ref;
+
+  Future<Cart> _fetchCart() {
+    final user = ref.read(authRepositoryProvider).currentUser;
+    if (user != null) {
+      return ref.read(remoteCartRepositoryProvider).fetchCart(user.uid);
+    } else {
+      return ref.read(localCartRepositoryProvider).fetchCart();
+    }
+  }
+
+  Future<void> _setCart(Cart cart) async {
+    final user = ref.read(authRepositoryProvider).currentUser;
+    if (user != null) {
+      await ref.read(remoteCartRepositoryProvider).setCart(user.uid, cart);
+    } else {
+      await ref.read(localCartRepositoryProvider).setCart(cart);
+    }
+  }
+
+  Future<void> setItem(Item item) async {
+    final cart = await _fetchCart();
+    final updated = cart.setItem(item);
+    await _setCart(updated);
+  }
+
+  Future<void> addItem(Item item) async {
+    final cart = await _fetchCart();
+    final updated = cart.addItem(item);
+    await _setCart(updated);
+  }
+
+  Future<void> removeItembyId(ProductID productId) async {
+    final cart = await _fetchCart();
+    final updated = cart.removeItemById(productId);
+    await _setCart(updated);
+  }
+}
+
+final cartServiceProvider = Provider<CartService>((ref) {
+  return CartService(ref);
+});
