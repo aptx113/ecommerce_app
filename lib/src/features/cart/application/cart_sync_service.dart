ddf059ded6ecec45002df4ad035e8c85d245d430
George <george.hs@hsress.com>
2022-11-30 00:17:26 +0800

    Implement logic to take product quantity into account when move local to remote

--- lib/src/features/cart/application/cart_sync_service.dart
+++ lib/src/features/cart/application/cart_sync_service.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:math';
 
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/models/app_user.dart';
-
-// part 'cart_sync_service.g.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/models/cart.dart';
+import 'package:ecommerce_app/src/features/cart/models/item.dart';
+import 'package:ecommerce_app/src/features/cart/models/mutable_cart.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CartSyncService {
   CartSyncService(this.ref) {
@@ -18,9 +22,49 @@ class CartSyncService {
       final previousUser = previous?.value;
       final currentUser = next.value;
       if (previousUser == null && currentUser != null) {
-        debugPrint('signed in: ${currentUser.uid}');
+        _moveItemsToRemoteCart(currentUser.uid);
       }
     });
+  }
+
+  Future<void> _moveItemsToRemoteCart(String uid) async {
+    try {
+      final localRepo = ref.read(localCartRepositoryProvider);
+      final localCart = await localRepo.fetchCart();
+      if (localCart.items.isNotEmpty) {
+        final remoteRepo = ref.read(remoteCartRepositoryProvider);
+        final remoteCart = await remoteRepo.fetchCart(uid);
+        final localItemsToAdd =
+            await _getLocalItemsToAdd(localCart, remoteCart);
+        final updatedCart = remoteCart.addItems(localItemsToAdd);
+
+        await remoteRepo.setCart(uid, updatedCart);
+        await localRepo.setCart(const Cart());
+      }
+    } catch (e) {
+      // TODO: Handle error and/or rethrow
+    }
+  }
+
+  Future<List<Item>> _getLocalItemsToAdd(
+      Cart localCart, Cart remoteCart) async {
+    final productsRepo = ref.read(productsRepositoryProvider);
+    final products = await productsRepo.fetchProductsList();
+    final localItemsToAdd = <Item>[];
+    for (final localItem in localCart.items.entries) {
+      final productId = localItem.key;
+      final localQuantity = localItem.value;
+
+      final remoteQuantity = remoteCart.items[productId] ?? 0;
+      final product = products.firstWhere((product) => product.id == productId);
+      final cappedLocalQuantity =
+          min(localQuantity, product.availableQuantity - remoteQuantity);
+      if (cappedLocalQuantity > 0) {
+        localItemsToAdd
+            .add(Item(productId: productId, quantity: cappedLocalQuantity));
+      }
+    }
+    return localItemsToAdd;
   }
 }
 
